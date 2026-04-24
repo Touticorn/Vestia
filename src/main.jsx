911d2571706902cf3bea2c44a7029fa62e07ab28
Touticorn <oudili.anass@gmail.com>
2026-04-24 12:02:30 +0100

    Fix styles rename

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Vestia from './Vestia.jsx'
-import './index.css'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
